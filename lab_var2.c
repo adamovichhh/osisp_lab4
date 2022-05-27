3ff98fcc977cd8af85b8eb68fef7b86926a77863
adamovichhh <93352018+adamovichhh@users.noreply.github.com>
2022-05-27 08:54:15 +0300

    Update lab_var2.c

--- lab_var2.c
+++ lab_var2.c
@@ -1,435 +1,191 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 #include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <signal.h>
-
-#include <sys/time.h>
+#include <wait.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
 #include <time.h>
 
+#define PAIRS_COUNT 9
+#define PROCESSES_COUNT 9
+#define NOSIG -1
+#define SIGDEF -2
 
-#define FORMAT "N:%d - pid:%d - ppid:%d- %s - %s - time:%ld \n"
-#define EX "N:%d - pid:%d - ppid:%d - ended work after %d sig SIGUSR1 and %d sig SIGUSR2\n"
-#define SLEEPTIME 1000
+typedef struct tagPair {
+    int create;
+    int signal;
+    int from;
+    int to;
+} Pair;
 
+Pair pairs[] = {
+    {1, NOSIG, 0, 1},
+    {1, SIGUSR1, 1, 2},
+    {1, SIGUSR1, 1, 3},
+    {1, SIGUSR1, 1, 4},
+    {1, SIGUSR2, 2, 5},
+    {1, SIGUSR2, 2, 6},
+    {1, SIGUSR1, 6, 7},
+    {1, SIGUSR1, 7, 8},
+    {0, SIGUSR2, 8, 1}
+};
 
+pid_t pids[PROCESSES_COUNT];
+int nodeIndex;
 
-int N=0,X=0,Y=0,GET=0;
-pid_t adress;
+int sendedSignalsCount[2];
 
+void sigusr(int);
+void sigterm();
+int signalChildren(int index, int sig);
+void outputInfo(char *status, int sigNum);
+void outputHeader();
 
-long int getTime()
-{
-	struct timeval time;
-	if (gettimeofday(&time, NULL) == -1)
-		perror("Can not get current time\n");
-	else {
-		return time.tv_sec * 1000 + time.tv_usec / 1000;
-	}
+int main() {
+    pids[0] = getpid();
+
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+
+    int pgid = 0;
+
+    for (int i = 0; i < PAIRS_COUNT; ++i) {
+        if(i == 0) {
+            pgid = 0;
+        }
+
+        if(nodeIndex == pairs[i].to && pairs[i].signal != NOSIG) {
+            signal(pairs[i].signal, sigusr);
+        }
+        
+        
+
+        if(nodeIndex == pairs[i].from && pairs[i].create) {
+        
+            //if ( nodeIndex != 0 ) { while (pairs[i - 1].create) { } }
+            while (pids[nodeIndex] == 0) {}
+            
+            pid_t pid;
+            switch (pid = fork()) {
+                case -1: {
+                    break;
+                }
+                case 0: {
+                    
+                    nodeIndex = pairs[i].to;
+                    pids[nodeIndex] = getpid();
+                    i = -1;
+
+                    signal(SIGTERM, sigterm);
+                    break;
+                }
+                default: {
+                    if(!pgid)
+                        pgid = pid;
+                    setpgid(pid, pgid);
+                    pids[pairs[i].to] = pid;
+
+                    break;
+                }
+            }
+        }
+    }
+    if(nodeIndex == 0)
+        outputHeader();
+
+    sleep(1);
+
+    // start point
+    if(nodeIndex == 1){
+        int sig = signalChildren(nodeIndex, SIGDEF);
+        if(sig != NOSIG) {
+            int sigNum = sig == SIGUSR1 ? 1 : 2;
+            sendedSignalsCount[sigNum - 1]++;
+        }
+    }
+
+    if(nodeIndex == 0){
+        getchar();
+        while(wait(NULL) != -1);
+        return 0;
+    }
+    else {
+        while(1)
+            pause();
+    }
 }
 
-void sigUsr1(int sig,siginfo_t *info,void *context){
+void sigusr(int sig) {
+    outputInfo("get", sig == SIGUSR1 ? 1 : 2);
 
-	if(sig!=SIGUSR1){
-			return;}
-		else{
-			GET++;
-			 if(GET<101){
-				 printf(FORMAT,N,getpid(),getppid(),"get","SIGUSR1",getTime());
-				 printf(FORMAT,N,getpid(),getppid(),"put","SIGUSR2",getTime());
-				 Y=Y+4;
-				 usleep(SLEEPTIME);
-				 if (kill(adress, SIGUSR2) == -1)
-					 perror("Can not send signal\n");
-			 }
-			 else{
-			 	int status;
-			 	usleep(SLEEPTIME*2);
+    if(nodeIndex == 1 && (sendedSignalsCount[0] + sendedSignalsCount[1]) == 101) {
+        kill(getpid(), SIGTERM);
+        return;
+    }
 
-				if (kill(adress, SIGTERM) == -1)
-					perror("Can not send signal\n");
-
-				for(int i=0;i<7;i++)
-			 	wait(&status);
-			 	
-			 	printf(EX,N,getpid(),getppid(),X,Y);
-			 	exit(0);
-			 	
-			 	
-			 }
-		}
+    int signal = signalChildren(nodeIndex, SIGDEF);
+    if(signal != NOSIG) {
+        int sigNum = signal == SIGUSR1 ? 1 : 2;
+        outputInfo("put", sigNum);
+        sendedSignalsCount[sigNum - 1]++;
+    }
 }
 
+int tryToGetChildrenPGID(int index, pid_t *pgid, int *sig) {
+    for (int i = 0; i < PAIRS_COUNT; ++i) {
+        if (pairs[i].from == index) {
+            *pgid = getpgid(pids[pairs[i].to]);
+            if(sig != NULL)
+                *sig = pairs[i].signal;
+            return 1;
+        }
+    }
 
-
-void sigUsr234(int sig,siginfo_t *info,void *context)
-{
-	if(sig==SIGTERM){
-	int status;
-		
-		if (kill(adress, SIGTERM) == -1)
-		perror("Can not send signal\n");
-		waitpid(adress,&status,0);
-		printf(EX,N,getpid(),getppid(),X,Y);
-		exit(0);
-	}
-
-	if(sig!= SIGUSR2){
-				return;
-			}
-			else{			
-				printf(FORMAT,N,getpid(),getppid(),"get","SIGUSR2",getTime());
-				printf(FORMAT,N,getpid(),getppid(),"put","SIGUSR1",getTime());
-				X++;
-				usleep(SLEEPTIME);
-				if (kill(adress, SIGUSR1) == -1)
-					perror("Can not send signal\n");
-			}
+    return 0;
 }
 
-void sigUsr5(int sig,siginfo_t *info,void *context)
-{
-	if(sig==SIGTERM){
-		printf(EX,N,getpid(),getppid(),X,Y);
-		exit(0);
-	}
+int signalChildren(int index, int sig) {
+    int s;
+    pid_t pgid;
+    if(tryToGetChildrenPGID(index, &pgid, &s)) {
+        int signal = sig == SIGDEF ? s : sig;
+        kill(-pgid, signal);
+        return signal;
+    }
 
-	if(sig!= SIGUSR2){
-				return;
-			}
-			else{
-			 	printf(FORMAT,N,getpid(),getppid(),"get","SIGUSR2",getTime());
-			 }
+    return NOSIG;
 }
 
-void sigUsr67(int sig,siginfo_t *info,void *context)
-{
-	if(sig==SIGTERM){
-		printf(EX,N,getpid(),getppid(),X,Y);
-		exit(0);
-	}
-
-	if(sig!= SIGUSR1){
-				return;
-			}
-			else{
-			 	printf(FORMAT,N,getpid(),getppid(),"get","SIGUSR1",getTime());
-			 }
+void outputHeader() {
+    printf("INDEX PID   PPID  STATUS SIGNAL TIME\n");
 }
 
-void sigUsr8(int sig,siginfo_t *info,void *context)
-{
-	if(sig==SIGTERM){
-		printf(EX,N,getpid(),getppid(),X,Y);
-		exit(0);
-	}
-	
+void outputInfo(char *status, int sigNum) {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    time_t sec = ts.tv_sec;
+    int ms = ts.tv_nsec / 1e6;
 
-	if(sig!= SIGUSR1){
-				return;
-			}
-			else{
-			 	printf(FORMAT,N,getpid(),getppid(),"get","SIGUSR1",getTime());
-			 	printf(FORMAT,N,getpid(),getppid(),"put","SIGUSR1",getTime());
-			 	X++;
-			 	usleep(SLEEPTIME);
-				if (kill(adress, SIGUSR1) == -1)
-					perror("Can not send signal\n");
-			 }
+    struct tm *tmp = localtime(&sec);
+
+    printf("%-5d %-5d %-5d %6s   USR%1d %02i:%02i:%02i:%03i\n",
+           nodeIndex, getpid(), getppid(), status, sigNum,
+           tmp->tm_hour, tmp->tm_min, tmp->tm_sec, ms);
+    fflush(stdout);
 }
 
-void sigfunc1(int sig,siginfo_t *info,void *context)
-{
-	printf("%d\n",N);
-}
+void sigterm() {
+    if (nodeIndex == 1) { system("ps --forest"); }
+    printf("%-5d %-5d %-5d has terminated. Sended: %4d SIGUSR1 and %4d SIGUSR2\n",
+           nodeIndex, getpid(), getppid(), sendedSignalsCount[0], sendedSignalsCount[1]);
 
+    pid_t pgid;
+    if(tryToGetChildrenPGID(nodeIndex, &pgid, NULL)) {
+        kill(-pgid, SIGTERM);
+        
+        waitpid(-pgid, NULL, 0);
+    }
 
-int main(int argc,char* argv[]){
-
-pid_t p;
-struct sigaction act={0};
-
-p=fork();
-if(p==0){
-	//перевода текущего процесса во вновь создаваемую группу процессов, идентификатор которой будет совпадать с идентификатором текущего процесса
-	setpgid(getpid(),getpid());
-	p=fork();
-	if(p>0){
-		p=fork();
-		if(p>0){
-			p=fork();
-			if(p>0){
-				p=fork();
-				if(p>0){//CHILD1
-					adress=-getpgid(getpid());
-					N=1;
-					act.sa_flags=SA_SIGINFO;
-					act.sa_sigaction=&sigUsr1;
-
-					if (sigaction(SIGUSR1, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-
-					if (sigaction(SIGUSR2, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-
-					if (sigaction(SIGTERM, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-					
-					usleep(SLEEPTIME*1000);
-					Y=Y+4;
-					
-					if (kill(adress, SIGUSR2) == -1)
-						perror("Can not send signal\n");
-					
-					
-					
-				}//~CHILD1
-				else if(p==0){ //CHILD5
-					N=5;
-					act.sa_flags=SA_SIGINFO;
-					act.sa_sigaction=&sigUsr5;
-					
-					if (sigaction(SIGUSR1, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-
-					if (sigaction(SIGUSR2, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-
-					if (sigaction(SIGTERM, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-					
-				}//~CHILD5
-				else if (p == -1) {
-					perror("Error: can not create  process");
-					exit(-1);
-				}
-			
-			}
-			else if(p==0){
-				p=fork();
-				if(p>0){//CHILD4
-					adress=p;
-					N=4;
-					act.sa_flags=SA_SIGINFO;
-					act.sa_sigaction=&sigUsr234;
-					
-					if (sigaction(SIGUSR1, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-
-					if (sigaction(SIGUSR2, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-
-					if (sigaction(SIGTERM, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-					
-				
-				}//~CHILD4
-				else if(p==0){//CHILD8
-					adress=getpgid(getpid());
-					setpgid(getpid(),getpid());
-					N=8;
-					act.sa_flags=SA_SIGINFO;
-					act.sa_sigaction=&sigUsr8;
-					
-					if (sigaction(SIGUSR1, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-
-					if (sigaction(SIGUSR2, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-
-					if (sigaction(SIGTERM, &act, NULL) == -1) {
-						perror("Error: can not set signal handler");
-						exit(-1);
-					}
-					
-					
-				}//~CHILD8
-				else if (p == -1) {
-					perror("Error: can not create  process");
-					exit(-1);
-				}
-}
-			else if (p == -1) {
-			perror("Error: can not create  process");
-			exit(-1);
-			}
-		}
-		else if(p==0){ 
-			p=fork();
-			if(p>0){//CHILD3
-				adress=p;
-				N=3;
-				act.sa_flags=SA_SIGINFO;
-				act.sa_sigaction=&sigUsr234;
-				
-				if (sigaction(SIGUSR1, &act, NULL) == -1) {
-					perror("Error: can not set signal handler");
-					exit(-1);
-				}
-
-				if (sigaction(SIGUSR2, &act, NULL) == -1) {
-					perror("Error: can not set signal handler");
-					exit(-1);
-				}
-
-				if (sigaction(SIGTERM, &act, NULL) == -1) {
-					perror("Error: can not set signal handler");
-					exit(-1);
-				}
-				
-			
-			}//~CHILD3
-			else if(p==0){//CHILD7
-				
-				setpgid(getpid(),getpid());
-				N=7;
-				act.sa_flags=SA_SIGINFO;
-				act.sa_sigaction=&sigUsr67;
-				
-				if (sigaction(SIGUSR1, &act, NULL) == -1) {
-					perror("Error: can not set signal handler");
-					exit(-1);
-				}
-
-				if (sigaction(SIGUSR2, &act, NULL) == -1) {
-					perror("Error: can not set signal handler");
-					exit(-1);
-				}
-
-				if (sigaction(SIGTERM, &act, NULL) == -1) {
-					perror("Error: can not set signal handler");
-					exit(-1);
-				}
-				
-				
-			}//~CHILD7
-			else if (p == -1) {
-				perror("Error: can not create  process");
-				exit(-1);
-			}
-		}
-		else if (p == -1) {
-		perror("Error: can not create  process");
-		exit(-1);
-		}
-	}
-	else if(p==0){ 
-		p=fork();
-		if(p>0){//CHILD2
-			adress=p;
-			N=2;
-			act.sa_flags=SA_SIGINFO;
-			act.sa_sigaction=&sigUsr234;
-			
-			if (sigaction(SIGUSR1, &act, NULL) == -1) {
-				perror("Error: can not set signal handler");
-				exit(-1);
-			}
-
-			if (sigaction(SIGUSR2, &act, NULL) == -1) {
-				perror("Error: can not set signal handler");
-				exit(-1);
-			}
-
-			if (sigaction(SIGTERM, &act, NULL) == -1) {
-				perror("Error: can not set signal handler");
-				exit(-1);
-			}
-			
-		
-		}//~CHILD2
-		else if(p==0){//CHILD6
-			
-			setpgid(getpid(),getpid());
-			N=6;
-			act.sa_flags=SA_SIGINFO;
-			act.sa_sigaction=&sigUsr67;
-			
-			if (sigaction(SIGUSR1, &act, NULL) == -1) {
-				perror("Error: can not set signal handler");
-				exit(-1);
-			}
-
-			if (sigaction(SIGUSR2, &act, NULL) == -1) {
-				perror("Error: can not set signal handler");
-				exit(-1);
-			}
-
-			if (sigaction(SIGTERM, &act, NULL) == -1) {
-				perror("Error: can not set signal handler");
-				exit(-1);
-			}
-
-		}//~CHILD6
-
-	}
-	else if (p == -1) {
-	perror("Error: can not create  process");
-	exit(-1);
-	}
-
-	
-}
-else if(p>0){//PARENT0
-	act.sa_flags=SA_SIGINFO;
-	act.sa_sigaction=&sigfunc1;
-	
-	if (sigaction(SIGUSR1, &act, NULL) == -1) {
-		perror("Error: can not set signal handler");
-		exit(-1);
-	}
-
-	if (sigaction(SIGUSR2, &act, NULL) == -1) {
-		perror("Error: can not set signal handler");
-		exit(-1);
-	}
-
-	if (sigaction(SIGTERM, &act, NULL) == -1) {
-		perror("Error: can not set signal handler");
-		exit(-1);
-	}
-	
-	int status;
-	waitpid(p,&status,0);
-	printf(EX,N,getpid(),getppid(),X,Y);
-	exit(0);
-	
-	
-	
-}else if(p == -1){
-perror("Error: can not create  process");
-exit(-1);
-}
-//~PARENT0
-
-int i=0;
-while(1){i++;}
-
-
-
+    exit(0);
 }
